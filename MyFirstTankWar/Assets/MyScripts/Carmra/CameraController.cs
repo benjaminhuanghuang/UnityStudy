8c528f074cd0c2f1e0efd1ba347f9fa68648fd4c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-02-26 20:14:38 -0800

    lesson 1 move tank

---
+++ MyFirstTankWar/Assets/MyScripts/Carmra/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject tank;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position;
+        MoveCamera();
+    }
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+    void MoveCamera()
+    {
+        transform.position = tank.transform.position + offset;
+    }
+
+}
---
+++ MyFirstTankWar/Assets/MyScripts/Tank/TankController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keeping press key
+        if (Input.GetKey(KeyCode.W))
+        {
+            // length of each frame
+            // dir*speed * time of frame
+            transform.Translate(new Vector3(0.00f, 0.0f, 1.0f) * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0.00f, 0.0f, -1.0f) * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //turn left (y)
+            transform.Rotate(new Vector3(0.00f, -1.0f, 0.0f) * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //turn right
+            transform.Rotate(new Vector3(0.00f, 1.0f, 0.0f) * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
